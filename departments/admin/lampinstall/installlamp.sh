75bd61d7945434213a542f0034f93692d04d6eb8
Matthew Evan <mattmadness@420blaze.it>
2021-11-13 23:25:24 -0500

    Edited youtubedl, added countryradio, translated butleryad to spanish and switched to a basic list.

--- departments/admin/lampinstall/installlamp.sh
+++ departments/admin/lampinstall/installlamp.sh
@@ -2,12 +2,13 @@
 pacman -S --noconfirm apache mariadb mariadb-clients libmariadbclient php php-apache
 sed -i 's/LoadModule unique_id_module modules\/mod_unique_id.so/#LoadModule unique_id_module modules\/mod_unique_id.so/g' /etc/httpd/conf/httpd.conf
 mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-systemctl enable httpd
-systemctl start httpd
-systemctl enable mysqld
-systemctl start mysqld
 sed -i 's/LoadModule mpm_event_module module\/mod_mpm_event.so/#LoadModule mpm_event_module module\/mod_mpm_event.so/g' /etc/httpd/conf/httpd.conf
 echo "LoadModule mpm_prefork_module modules/mod_mpm_prefork.so" >> /etc/httpd/conf/httpd.conf
 echo "LoadModule php7_module modules/libphp7.so" >> /etc/httpd/conf/httpd.conf
 echo "AddHandler php7-script php" >> /etc/httpd/conf/httpd.conf
 echo "Include conf/extra/php7_module.conf" >> /etc/httpd/conf/httpd.conf
+systemctl enable httpd
+systemctl start httpd
+systemctl enable mysqld
+systemctl start mysqld
+
--- departments/prod/butleryad/task.sh
+++ departments/prod/butleryad/task.sh
@@ -4,21 +4,24 @@ declare -a tasks=($(find /usr/share/sapphire-butler/departments/ -iname task.sh 
 len=${#tasks[@]}
 formattasklist() {
 	for (( i = 0; i < len; i++ )); do
-		if [ -f "/usr/share/sapphire-butler/i18n/$1/$BLANG" ]; then
-			source /usr/share/sapphire-butler/i18n/$1/$BLANG
-		elif [ -f "/usr/share/sapphire-butler/i18n/$1/en" ]; then
-			source /usr/share/sapphire-butler/i18n/$1/en
+		if [ -f "/usr/share/sapphire-butler/i18n/${tasks[$i]}/${BLANG}" ]; then
+			source /usr/share/sapphire-butler/i18n/${tasks[$i]}/$BLANG
+		elif [ -f "/usr/share/sapphire-butler/i18n/${tasks[$i]}/en" ]; then
+			source /usr/share/sapphire-butler/i18n/${tasks[$1]}/en
 		else
-			source /usr/share/sapphire-butler/i18n/$1/$(find -type f /usr/share/sapphire-butler/i18n/$1/ | head -1)
+			source /usr/share/sapphire-butler/i18n/${tasks[$1]}/$(find -type f /usr/share/sapphire-butler/i18n/${tasks[$1]}/ | head -1)
 		fi
-		echo "FALSE "
 		echo "$name "
 		echo "${tasks[$i]} "
 		echo "$description "
 	done
 }
-declare -a do=($(formattasklist | yad --list --title="$taskname" --window-icon="/usr/share/sapphire-butler/butlerface.png" --geometry=800x700 --text="$yadtext" --column="Do?" --column="Name" --column="Department/Task" --column="Description" --checklist | cut -d '|' -f3))
+declare -a do=($(formattasklist | yad --list --title="$taskname" --window-icon="/usr/share/sapphire-butler/butlerface.png" --geometry=800x700 --text="$yadtext" --column="Name" --column="Department/Task" --column="Description" | cut -d '|' -f2))
 len=${#do[@]}
 for (( i = 0; i < len; i++ )); do
 	butler ${do[$i]}
 done
+yad --title="$taskname" --window-icon="/usr/share/sapphire-butler/butlerface.png" --text="$yadtextdomore" --button="No":2 --button="Yes":3
+if [ "$?" == "3" ]; then
+	butler prod/butleryad
+fi
